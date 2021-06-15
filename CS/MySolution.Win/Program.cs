46a73a18dc3601a3ff82a37f7b8890d799250bcb
Andrey <andrey.kozhevnikov@devexpress.com>
2021-06-15 09:27:45 +0300

    win modern look

--- CS/MySolution.Win/Program.cs
+++ CS/MySolution.Win/Program.cs
@@ -16,7 +16,7 @@ namespace MySolution.Win {
         /// </summary>
         [STAThread]
         static void Main() {
-            DevExpress.ExpressApp.FrameworkSettings.DefaultSettingsCompatibilityMode = DevExpress.ExpressApp.FrameworkSettingsCompatibilityMode.v20_1;
+            DevExpress.ExpressApp.FrameworkSettings.DefaultSettingsCompatibilityMode = DevExpress.ExpressApp.FrameworkSettingsCompatibilityMode.Latest;
 #if EASYTEST
             DevExpress.ExpressApp.Win.EasyTest.EasyTestRemotingRegistration.Register();
 #endif
